fb4e8df3bd270299688b6f7422e6185034d8a62a
ha.bui <ha.bui@s60.com.au>
2016-03-23 01:13:46 +0700

    TeST

--- src/main/java/ca/pjer/rabbitmqclient/ScheduleTask.java
+++ src/main/java/ca/pjer/rabbitmqclient/ScheduleTask.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,9 @@ public class ScheduleTask {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private SimpMessagingTemplate template;
+
     // this will send a message to an endpoint on which a client can subscribe
     @Scheduled(fixedRate = 5000)
     public void trigger() {
@@ -27,7 +31,7 @@ public class ScheduleTask {
         if(msg != null){
             String messageStr = new String(msg.getBody());
             String formatMsg = MessageFormat.format("{0}> {1}", environment.getProperty("HOSTNAME"), messageStr);
-            this.rabbitTemplate.convertAndSend("/topic/message", formatMsg);
+            this.template.convertAndSend("/topic/message", formatMsg);
         }
 
     }
